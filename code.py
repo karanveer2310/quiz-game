add815e1c9a31ae7d7f3083f59a5d93b34343165
karanveer2310 <99191646+karanveer2310@users.noreply.github.com>
2022-06-19 12:35:52 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
 
+
+print("...........TEACHER CONSOLE..........")
+
+def add_ques():
+        q = input("Enter question:")
+        o1 = input("Enter option 1")
+        o2 = input("Enter option 2")
+        o3 = input("Enter option 3")
+        o4 = input("Enter option 4")
+
+def delete_ques (char *ques):
+    del ques
+
+
+
